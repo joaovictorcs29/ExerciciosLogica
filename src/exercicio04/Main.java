a72c6097b31dbd8f4c59bdaf6491f20ac399c567
João Victor Costa <joaovictorcs29@gmail.com>
2025-02-23 12:20:02 -0300

    Exercicio04

---
+++ src/exercicio04/Main.java
@@ -0,0 +1,11 @@
+package exercicio04;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 30;
+
+        System.out.print("Número: " + a + " | ");
+        System.out.print("Antecessor = " + (a - 1) + " | ");
+        System.out.println("Sucessor = " + (a + 1));
+    }
+}
