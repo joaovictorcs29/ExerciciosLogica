28bc4c1e93052dcabd6425f84d60d89764a8ec92
João Victor Costa <joaovictorcs29@gmail.com>
2025-02-23 13:17:55 -0300

    Exercicio 06

---
+++ src/exercicio06/Main.java
@@ -0,0 +1,9 @@
+package exercicio06;
+
+public class Main {
+    public static void main(String[] args) {
+        double valor = 20;
+
+        System.out.println("Valor com 5% de reajuste: " + (valor + valor * 0.05));
+    }
+}
