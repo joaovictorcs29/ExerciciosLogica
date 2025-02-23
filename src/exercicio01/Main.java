efe2a7bcf107e20031d2143b0008705ba913140a
João Victor Costa <joaovictorcs29@gmail.com>
2025-02-22 23:01:40 -0300

    Exercicio 01

---
+++ src/exercicio01/Main.java
@@ -0,0 +1,12 @@
+package exercicio01;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Valores valores = new Valores(1,2,3);
+
+        valores.somaDoisNumeros(valores.getA(), valores.getB());
+
+    }
+}
---
+++ src/exercicio01/Valores.java
@@ -0,0 +1,47 @@
+package exercicio01;
+
+public class Valores {
+    private int a;
+    private int b;
+    private int c;
+
+    public Valores(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public void somaDoisNumeros(int a,int b){
+        int soma = this.a + this.b;
+        System.out.println("Soma: " + soma);
+        if (soma < this.c ){
+            System.out.println("A soma entre a: " + a + " + b: " + b + " = " + soma + " e menor que c: " + c);
+        }else if( soma == this.c ){
+            System.out.println("A soma entre a: " + a + " + b: " + b + " = " + soma + " e igual a c: " + c);
+        }else System.out.println("A soma entre a: " + a + " + b: " + b + " = " + soma + " e maior a c: " + c);
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+}
